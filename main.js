fe2d6d4a03eb1b6070dbd9ef36277adcddc41be7
Luis <s.ros.lui@gmail.com>
2020-11-05 19:53:51 +0100

    .

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
     console.log("Icone display");
 
+    const colori = ['blue','orange','purple'];
+
     // icone set da importare
     // ogni elemento dell' array 'icons' ha proprietà: na,e, prefix, type, family
     const icons = [
@@ -102,10 +104,33 @@ $(document).ready(function(){
         },
     ];
 
+    
     // riferimento DOM .icone
     var boxContenitore = $('.icone');
 
-    stampaIcone(icons, boxContenitore);
+    /*chiamata funzione */
+    // stampaIcone(icons, boxContenitore);
+
+    /*stampa con le icone colorate */
+    const iconeColorate = iconeColor(icons, colori);
+    console.log(iconeColorate);
+    stampaIcone(iconeColorate, boxContenitore);
+
+    /** barra  dei filtri */
+    const select = $('#type');
+    const tipi = prendiTipo(icons);
+
+    // generazione di opzioni
+    genOpt(tipi, select);
+
+    // cambio dell' evento
+    select.change( () => {
+        const selected = select.val();
+        console.log(selected);
+
+        const filteredIcone = iconeFiltrate(iconeColorate, selected);
+        stampaIcone(filteredIcone, boxContenitore);
+    });
 
 
 
@@ -114,9 +139,78 @@ $(document).ready(function(){
 
 
 
-/**
- * ARRIVATO QUI, MERDA INIZIA DA QUIIIIIII
- */
+/** la funzione stampa a schermo le img delle icone contenenti all' array icona */ 
+function stampaIcone(arrayIcone, box){
 
- // la funzione stampa in output le icone
-// function stampaIcone(arrayIcone, box){}
+    // reset dell' output
+    box.html("");
+
+    // fa il loop a ciascun elemento dell' arrayIcone, stampa ogni singolo elemento icona
+    arrayIcone.forEach((icona) => {
+        const {family, prefix, name, color} = icona;
+
+        /* gli apici `` sono quelli 
+        * facendo alt + 96
+        * si chiamano Backtick    */
+
+        const html =                /* ${ } interpolazione*/   
+        `<div class="icona">             
+            <i class="${family} ${prefix}${name}" 
+            style="color: ${color}"></i>     
+            <div class="nome">${name}</div>     
+        </div>`;
+
+        box.append(html);
+    });
+}
+
+
+/* funzione restituisce colore associato al tipo dell' icona*/
+function iconeColor(arrayIcone, colori){
+
+    // prendere il tipo delle icone
+    const tipo = prendiTipo(arrayIcone);
+    console.log(tipo);
+
+    // assegnare un colore ad ogni tipo
+    const iconeColorate = arrayIcone.map( (icona) =>{
+        const indiceTipo = tipo.indexOf(icona.type);
+        return {
+            ...icona,
+            color: colori[indiceTipo]
+        };
+    });
+    return iconeColorate;
+
+}
+
+function prendiTipo(arrayIcone){
+    const tipo = [];
+
+    arrayIcone.forEach(icona => {
+        if(! tipo.includes(icona.type) ){
+            tipo.push(icona.type);
+        }
+    });
+    return tipo;
+}
+
+/** funzione genera le opzioni della barra filtri */
+function genOpt(tipi, select){
+    tipi.forEach( (opt) => {
+        select.append(`<option value="${opt}">${opt}</option>`);
+    });
+}
+
+function iconeFiltrate(iconeColorate, selected){
+
+    if(selected === "all"){
+        return iconeColorate;
+    }
+
+    const filtered  = iconeColorate.filter( (icona) => {
+        return icona.type === selected;
+    });
+
+    return filtered;
+}
